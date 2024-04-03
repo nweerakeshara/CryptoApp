5cb052c6e31a6b695f11c6fa22f21a6ec0215ecd
nweerakeshara <thnuwanga@gmail.com>
2024-04-03 20:12:41 +1100

    footer added

--- FE/src/App.js
+++ FE/src/App.js
@@ -2,36 +2,46 @@
 //then navbar.js inside components folder
 
 import React from 'react';
-import {Routes, Route, Link} from 'react-router-dom';
-import {Layout, Typography, Space} from 'antd';
-import {Navbar, HomePage, Exchanges, CryptoCurrencies, CryptoDetails, News} from './components';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar, HomePage, Exchanges, CryptoCurrencies, CryptoDetails, News } from './components';
 import 'antd/dist/reset.css'
 import './App.css';
 
 const App = () => {
-  return (
-    <div className='app'>
-      <div className='navbar'>{/*navbar section*/}
-        <Navbar />      
-      </div>
-      <div className='main'>{/*main section*/}
-        <Layout>
-            <div className='routes'>
-                <Routes>
-                    <Route path='/' element={<HomePage/>} />
-                    <Route path='/exchanges' element={<Exchanges/>} />  
-                    <Route path='/cryptocurrencies' element={<CryptoCurrencies/>} />  
-                    <Route path='//crypto/:coinId' element={<CryptoDetails/>} />  
-                    <Route path='/news' element={<News/>} />                     
-                </Routes>
+    return (
+        <div className='app'>
+            <div className='navbar'>{/*navbar section*/}
+                <Navbar />
             </div>
-        </Layout>
-    </div>
-    <div className='footer'>{/*footer section*/}
-      
-    </div>
-    </div>
-  )
+            <div className='main'>{/*main section*/}
+                <Layout>
+                    <div className='routes'>
+                        <Routes>
+                            <Route path='/' element={<HomePage />} />
+                            <Route path='/exchanges' element={<Exchanges />} />
+                            <Route path='/cryptocurrencies' element={<CryptoCurrencies />} />
+                            <Route path='//crypto/:coinId' element={<CryptoDetails />} />
+                            <Route path='/news' element={<News />} />
+                        </Routes>
+                    </div>
+                </Layout>
+                <div className='footer'>{/*footer section*/}
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        Crypto News Hub <br />
+                        All Rights Reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to='/'>Home</Link>
+                        <Link to='/cryptocurrencies'>Crypto Currencies</Link>
+                        <Link to='/exchanges'>Exchanges</Link>
+                        <Link to='/news'>News</Link>
+                    </Space>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default App
--- FE/src/components/HomePage.js
+++ FE/src/components/HomePage.js
@@ -1,10 +1,31 @@
 //rafce
-import React from 'react'
+//in antd total num of cols is taken as 24. so 12 is half
+import React from 'react';
+import millify from 'millify';
+import {Link} from 'react-router-dom';
+import { Typography, Row, Col, Statistic } from 'antd';
 
 const HomePage = () => {
   return (
     <div>
-      HomePage
+        <Typography.Title level={2} className='heading'>Global Crypto Stats</Typography.Title>
+        <Row>
+            <Col span={12}>
+                <Statistic title='Total Cryptocurrencies' value={5} />
+            </Col>
+            <Col span={12}>
+                <Statistic title='Total Exchanges' value={5} />
+            </Col>
+            <Col span={12}>
+                <Statistic title='Total Market Cap' value={5} />
+            </Col>
+            <Col span={12}>
+                <Statistic title='Total 24h Volume' value={5} />
+            </Col>
+            <Col span={12}>
+                <Statistic title='Total Markets' value={5} />
+            </Col>
+        </Row>
     </div>
   )
 }
