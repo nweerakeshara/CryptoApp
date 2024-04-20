c7e2cff5a70b65262b796b7c01d066a920f0b1c9
nweerakeshara <thnuwanga@gmail.com>
2024-04-20 21:00:39 +1000

    home page crypto price completed

--- FE/src/components/HomePage.js
+++ FE/src/components/HomePage.js
@@ -5,29 +5,48 @@ import millify from 'millify';
 import {Link} from 'react-router-dom';
 import { Typography, Row, Col, Statistic } from 'antd';
 
+import { useGetCryptosQuery } from '../services/cryptoApi';
+import { CryptoCurrencies,News } from '../components';
+
+
 const HomePage = () => {
-  return (
-    <div>
-        <Typography.Title level={2} className='heading'>Global Crypto Stats</Typography.Title>
-        <Row>
-            <Col span={12}>
-                <Statistic title='Total Cryptocurrencies' value={5} />
-            </Col>
-            <Col span={12}>
-                <Statistic title='Total Exchanges' value={5} />
-            </Col>
-            <Col span={12}>
-                <Statistic title='Total Market Cap' value={5} />
-            </Col>
-            <Col span={12}>
-                <Statistic title='Total 24h Volume' value={5} />
-            </Col>
-            <Col span={12}>
-                <Statistic title='Total Markets' value={5} />
-            </Col>
-        </Row>
-    </div>
-  )
+    const {data, isFetching} = useGetCryptosQuery(12);
+    const globalStats = data?.data?.stats;
+   
+    return (
+        <div>
+            <Typography.Title level={2} className='heading'>Global Crypto Stats</Typography.Title>
+            <Row>
+                <Col span={12}>
+                    <Statistic title='Total Cryptocurrencies' value={globalStats?.total} />
+                </Col>
+                <Col span={12}>
+                    <Statistic title='Total Exchanges' value={millify(globalStats?.totalExchanges)} />
+                </Col>
+                <Col span={12}>
+                    <Statistic title='Total Market Cap' value={millify(globalStats?.totalMarketCap)} />
+                </Col>
+                <Col span={12}>
+                    <Statistic title='Total 24h Volume' value={millify(globalStats?.total24hVolume)} />
+                </Col>
+                <Col span={12}>
+                    <Statistic title='Total Markets' value={millify(globalStats?.totalMarkets)} />
+                </Col>
+            </Row>
+
+            <div className='home-heading-container'>
+                <Typography.Title level={2} className='home-title'>Top 10 Cryptocurrencies in the world</Typography.Title>
+                <Typography.Title level={3} className='show-more'><Link to='/cryptocurrencies'>Show More</Link></Typography.Title>
+            </div>
+            <CryptoCurrencies simplified /> {/*simplified keyword use to show only 10 */}
+
+            <div className='home-heading-container'>
+                <Typography.Title level={2} className='home-title'>Latest Crypto News</Typography.Title>
+                <Typography.Title level={3} className='show-more'><Link to='/news'>Show More</Link></Typography.Title>
+            </div>
+            <News simplified />
+        </div>
+    )
 }
 
 export default HomePage
--- FE/src/index.js
+++ FE/src/index.js
@@ -2,13 +2,19 @@
 //create 1st
 //then app.js
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './app/store';
 
-
-ReactDOM.render(
-    <Router>
-        <App/>
-    </Router>, document.getElementById('root'));
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <Router>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </Router>
+    </StrictMode>
+);
